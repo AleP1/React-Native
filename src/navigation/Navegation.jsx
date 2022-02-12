745976e5bf77d6c94a9ad32bc7b67de53f7ef09a
AleP1 <63692378+AleP1@users.noreply.github.com>
2022-02-12 02:01:16 -0300

    Tab Navegation

--- src/navigation/Navegation.jsx
+++ src/navigation/Navegation.jsx
@@ -1,16 +1,55 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable global-require */
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable quotes */
+import React from "react";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Favorite from "../screens/Favorite";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import FavoriteNavigation from "./FavoriteNavigation";
 import Account from "../screens/Account";
 import Pokedex from "../screens/Pokedex";
 
 const Tab = createBottomTabNavigator();
 
 export default function Navegation() {
+  const renderPokeball = () => (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{ width: 75, height: 75, top: -15 }}
+    />
+  );
+
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Favorite" component={Favorite} />
-      <Tab.Screen name="Account" component={Account} />
-      <Tab.Screen name="Pokedex" component={Pokedex} />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteNavigation}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Mi Cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+import React from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
